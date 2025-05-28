9d4bb8b17dbf0f964bb109fcb4e04e3c9c9e0235
Y-anYan <2543137@dundee.ac.uk>
2025-05-28 14:30:57 +0800

    Player movement
    Players can now move around on the map, and both the walls and the players have added colliders.

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     private Animator anim;
+    private float speed = 0.1f;
+    private Rigidbody2D rig;
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
+        rig = GetComponent<Rigidbody2D>();
     }
     private void Update()
     {
+        Move();
+    }
+
+    /// <summary>
+    /// move function
+    /// </summary>
+    private void Move()
+    {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-        print("h:" + h + "v:" +  v);
-
         anim.SetFloat("Horizontal", h);
         anim.SetFloat("Vertical", v);
+        rig.MovePosition(transform.position + new Vector3(h, v) * speed);
     }
 }
